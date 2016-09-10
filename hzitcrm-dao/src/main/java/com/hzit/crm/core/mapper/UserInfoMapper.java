248dc4c2265e5b72c5c08945558508654ed3e9c9
wuwenjie <wuwenjie@hzitxx.com>
2016-09-10 19:11:12 +0800

    CHUSHIHUA

---
+++ hzitcrm-dao/src/main/java/com/hzit/crm/core/mapper/UserInfoMapper.java
@@ -0,0 +1,24 @@
+package com.hzit.crm.core.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hzit.crm.core.entity.UserInfo;
+import org.apache.ibatis.annotations.Param;
+
+import com.fc.platform.commons.page.Page;
+import com.fc.platform.commons.page.Pageable;
+
+public interface UserInfoMapper {
+
+	void insertUserinfo(UserInfo userinfo);
+
+	void deleteUserinfoByUserid(Integer userid);
+
+	void updateUserinfo(UserInfo userinfo);
+
+	Page<UserInfo> searchUserinfoByParams(@Param("map") Map<String, String> map, Pageable pageable);
+
+	List<UserInfo> searchUserinfoByParams(@Param("map") Map<String, String> map);
+
+} 
