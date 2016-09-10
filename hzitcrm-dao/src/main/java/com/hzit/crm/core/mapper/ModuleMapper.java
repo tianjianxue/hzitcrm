203ef5d37395dc5026f3f807afae5f20b7e7f896
wuwenjie <wuwenjie@hzitxx.com>
2016-09-10 19:00:03 +0800

    CHUSHIHUA

---
+++ hzitcrm-dao/src/main/java/com/hzit/crm/core/mapper/ModuleMapper.java
@@ -0,0 +1,23 @@
+package com.hzit.crm.core.mapper.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+import com.hzit.crm.core.entity.Module;
+import com.fc.platform.commons.page.Page;
+import com.fc.platform.commons.page.Pageable;
+
+public interface ModuleMapper {
+
+	void insertModule(Module module);
+
+	void deleteModuleByModuleId(Integer moduleId);
+
+	void updateModule(Module module);
+
+	Page<Module> searchModuleByParams(@Param("map") Map<String, String> map, Pageable pageable);
+
+	List<Module> searchModuleByParams(@Param("map") Map<String, String> map);
+
+} 
---
+++ hzitcrm-dao/src/main/java/com/hzit/crm/core/mapper/RoleModuleMapper.java
@@ -0,0 +1,23 @@
+package com.hzit.crm.core.mapper.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+import com.hzit.crm.core.entity.RoleModule;
+import com.fc.platform.commons.page.Page;
+import com.fc.platform.commons.page.Pageable;
+
+public interface RoleModuleMapper {
+
+	void insertRoleModule(RoleModule roleModule);
+
+	void deleteRoleModuleById(Integer id);
+
+	void updateRoleModule(RoleModule roleModule);
+
+	Page<RoleModule> searchRoleModuleByParams(@Param("map") Map<String, String> map, Pageable pageable);
+
+	List<RoleModule> searchRoleModuleByParams(@Param("map") Map<String, String> map);
+
+} 
